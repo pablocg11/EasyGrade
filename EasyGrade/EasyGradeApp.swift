b5264a2a98c2d272af5ee6209800a740673e40d9
Pablo Castro <pablocastrodb@gmail.com>
2024-10-28 15:50:36 +0100

    Initial Commit

---
+++ EasyGrade/EasyGradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyGradeApp.swift
+//  EasyGrade
+//
+//  Created by Pablo Castro on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EasyGradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
